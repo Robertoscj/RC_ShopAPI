dc03f39d21d5e708d1401fd52a8d0f32be2623cb
robertomscjp <robertomscjp@gmail.com>
2023-07-27 20:27:08 -0300

    Implementação ICategoriaRepository

---
+++ RCShop.ProdutoAPI/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,14 @@
+﻿using RCShop.ProdutoAPI.Models;
+
+namespace RCShop.ProdutoAPI.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        Task<IEnumerable<Categoria>> GetAll();
+        Task<IEnumerable<Categoria>> GetCategoriasProdutos();
+        Task<Categoria>GetById(int id);
+        Task<Categoria> Create(Categoria categoria);
+        Task<Categoria> Update(Categoria categoria);
+        Task<Categoria> Delete(int id);
+    }
+}
