f3ecd963e251b88a3953061c66a0448571352586
robertomscjp <robertomscjp@gmail.com>
2023-07-27 19:54:31 -0300

    DTOs

---
+++ RCShop.ProdutoAPI/DTOs/CategoriaDTO.cs
@@ -0,0 +1,11 @@
+﻿using RCShop.ProdutoAPI.Models;
+
+namespace RCShop.ProdutoAPI.DTOs
+{
+    public class CategoriaDTO
+    {
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ RCShop.ProdutoAPI/DTOs/ProdutoDTO.cs
@@ -0,0 +1,22 @@
+﻿using RCShop.ProdutoAPI.Models;
+
+namespace RCShop.ProdutoAPI.DTOs
+{
+    public class ProdutoDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public decimal Preco { get; }
+
+        public string Descricao { get; }
+
+        public long Estoque { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public int CategoriaId { get; set; }
+    }
+}
